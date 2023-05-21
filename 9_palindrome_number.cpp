765b0cb8336bb29afacd58a67e632c80082a43b2
Dmitriy B <dbkkll@yandex.ru>
2023-05-21 20:12:58 +0500

    palindrome slight clean up

--- 9_palindrome_number.cpp
+++ 9_palindrome_number.cpp
@@ -1,52 +1,13 @@
 #include <iostream>
-#include <string>
 
 
-bool isPalindrome1(int x) {
-	if (x < 0) return false;
-	if (x < 10) return true;
-
-	std::string s = std::to_string(x);
-	const char* sc = s.c_str();
-	for (int i = 0; i < s.size() / 2; i++) {
-		if (sc[i] != sc[s.size() - 1 - i]) {
-			return false;
-		}
-	}
-	return true;
-}
-
-bool isPalindrome2(int x) {
-	if (x < 0) return false;
-	if (x < 10) return true;
-
-	std::string s = std::to_string(x);
-	const char* sc = s.c_str();
-	char* sr = new char[s.size()];
-
-	for (int i = s.size() - 1, j = 0; i >= 0; i--, j++) {
-		sr[j] = sc[i];
-	}
-
-	bool polindrome = true;
-	for (int i = 0; i < s.size(); i++) {
-		if (sr[i] != sc[i]) {
-			polindrome = false;
-		}
-	}
-
-	delete[] sr;
-
-	return polindrome;
-}
-
-bool isPalindrome(int x) {
+bool isPalindrome(const int x) {
     if (x < 0) return false;
-    int reversed = 0, original = x;
+    int reversed = 0, origin = x;
     do {
-        reversed = reversed * 10 + original % 10;
-        original /= 10;
-    } while(original != 0);
+        reversed = reversed * 10 + origin % 10;
+        origin /= 10;
+    } while(origin != 0);
     return reversed == x;
 }
 
