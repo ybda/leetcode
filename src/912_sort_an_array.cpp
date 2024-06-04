5ac42be22f153837253c9ba6d5704ccaa98b5912
Dmitriy B. <ybdapw@gmail.com>
2024-06-04 18:54:57 +0500

    Cleanup quicksort

--- src/912_sort_an_array.cpp
+++ src/912_sort_an_array.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "util.h"
 
-static void quicksort(int *target, int left, int right) {
+void quicksortInternal(int *target, int left, int right) {
     if(left >= right) return;
     int i = left, j = right;
     int tmp, pivot = target[i];
@@ -12,21 +12,27 @@ static void quicksort(int *target, int left, int right) {
         tmp = target[i]; target[i] = target[j]; target[j] = tmp;
         i++; j--;
     }
-    quicksort(target, left, i-1);
-    quicksort(target, j+1, right);
+    quicksortInternal(target, left, i - 1);
+    quicksortInternal(target, j + 1, right);
 }
 
-static void quicksort(int *target, int size) {
-    quicksort(target, 0, size - 1);
+void quicksort(int *target, int size) {
+    quicksortInternal(target, 0, size - 1);
+}
+
+int* sortArray(int* nums, int numsSize, int* returnSize) {
+    quicksort(nums, numsSize);
+    *returnSize = numsSize;
+    return nums;
 }
 
 int main() {
     int arr[] = {10, 7, 8, 7, 9, 1, 5};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    std::cout << "Original array: " << util::arrStr(arr, size) << "\n";
+    std::cout << "Original array: " << util::arrToStr(arr, size) << "\n";
 
     quicksort(arr, size);
 
-    std::cout << "Sorted array: " << util::arrStr(arr, size) << "\n";
+    std::cout << "Sorted array: " << util::arrToStr(arr, size) << "\n";
 }
